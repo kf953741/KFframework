dc29a7e56c8613878f17ac23b25d65e7ec20a462
kf953741 <761551096@qq.com>
2016-04-01 20:39:02 +0800

    unity UI框架
    unity UI框架

---
+++ Assets/Scripts/KFframework/BaseObject/BaseUI.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BaseUI : MonoBehaviour
+{
+
+    private GameObject cacheGameObject;
+    public GameObject CacheGameObject
+    {
+        get
+        {
+            if (null == cacheGameObject)
+            {
+                cacheGameObject = this.gameObject;
+
+            }
+            return cacheGameObject;
+        }
+    }
+
+    private Transform cacheTransform;
+    public Transform CacheTransform
+    {
+        get
+        {
+            if (null == cacheTransform)
+            {
+                cacheTransform = this.transform;
+            }
+            return cacheTransform;
+        }
+    }
+
+    protected EnumObjectState _state = EnumObjectState.None;
+
+    public event StateChangeEvent StateChanged;
+
+    public EnumObjectState State
+    {
+        protected get
+        {
+            return this._state;
+        }
+        set
+        {
+            EnumObjectState oldState = this._state;
+            this._state = value;
+            if (null != StateChanged)
+            {
+                StateChanged(this, this._state, oldState);
+            }
+
+        }
+    }
+
+    public abstract EnumUIType GetUIType();
+    // Use this for initialization
+
+    void Awake()
+    {
+        OnAwake();
+    }
+    void Start()
+    {
+        OnStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected virtual void OnAwake() { }
+    protected virtual void OnStart() { }
+}
---
+++ Assets/Scripts/KFframework/Defines/Defines.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum EnumObjectState
+{
+    None,
+    Initial,
+    Loading,
+    Ready,
+    Disable,
+    Closeing
+}
+
+public enum EnumUIType : int
+{
+    None = -1,
+    TestOne = 0,
+    TestTwo = 1,
+}
+
+public delegate void StateChangeEvent(Object ui, EnumObjectState newState, EnumObjectState oldState);
+
+public class Defines  {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
